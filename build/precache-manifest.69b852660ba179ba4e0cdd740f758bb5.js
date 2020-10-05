d622901db865e04e8a26bd068e130f53e7f248a6
Diego Rodrigues <diegocrodrigues.dev@gmail.com>
2020-10-05 01:41:10 -0300

    Deploy

---
+++ build/precache-manifest.69b852660ba179ba4e0cdd740f758bb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d2734448aea5671532a8be319f27a9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4090c0e3e6cfdf14a54f",
+    "url": "/static/js/2.65d1b90e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.65d1b90e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce87ea833e967c7e6aee",
+    "url": "/static/js/main.dac1b1b9.chunk.js"
+  },
+  {
+    "revision": "88e08b9ec711c230fcbb",
+    "url": "/static/js/runtime-main.63a657a4.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/static/media/github-background.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/static/media/logo.04f6798f.svg"
+  }
+]);
